e5f3e3897bf8d347bb42af863ca7410e2ec434b4
p47r1ck103 <p47r1ck.v@gmail.com>
2024-11-11 10:17:03 -0800

    last commit

--- src/com/coderscampus/assignment3/UserService.java
+++ src/com/coderscampus/assignment3/UserService.java
@@ -2,42 +2,47 @@
 package com.coderscampus.assignment3;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class UserService {
 
-	User[] usersArray = new User[100];
-			
-		BufferedReader fileReader=null( fileReader=new BufferedReader(new FileReader("filename"));
-	String line;
+	User[] usersArray = new User[4];
 
-	while((line=fileReader.readLine())!=null)
-	{
-		String[] parts = line.split(",");
+	public void readFile() {
+		try {
+			BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"));
 
-		if (parts.length == 3) {
-			String username = parts[0];
-			String password = parts[1];
-			String name = parts[2];
-//			users.add(new User(username, password, name)); needs changed
+			String line;
+			int counter = 0;
+			while ((line = fileReader.readLine()) != null) {
+
+				String[] parts = line.split(",");
+
+				if (parts.length == 3) {
+					String username = parts[0];
+					String password = parts[1];
+					String name = parts[2];
+					
+					usersArray[counter] = new User(username, password, name); 
+					System.out.println(usersArray[counter]);
+				}
+				counter++;
+			}
+		} catch (IOException e) {
+			System.out.println(e);
 		}
 	}
-//		filereader=new BufferedReader(new FileReader(data.txt)) {
-//			String line;
-//			while((line=reader.readline())!=null){
-//				String[] parts = line.split(",");
-//				if (parts.length == 3) {
-//					users.add(new User(parts[0], parts[1], parts[2]));
-//			}
-//		}
-	
 
-public static User validateLogin(String email, String password) {
-	User[] users;
-	for (User user : users) {
-		if (user.getUsername().equals(email) && user.getPassword().equals(password)) {
-			return user;
+
+		
+	public User validateLogin(String email, String password) {
+		
+		for (User user : usersArray) {
+			if (user.getUsername().equals(email) && user.getPassword().equals(password)) {
+				return user;
+			}
 		}
-	}
-	return null;
+		return null;
 	}
 }
